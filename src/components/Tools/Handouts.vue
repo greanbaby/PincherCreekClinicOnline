ea7206031bd634eaa32ffc2ed358ef835a9da683
Scott Gingras <greanbaby@gmail.com>
2023-05-05 15:51:27 -0600

    Updated handouts page

--- src/components/Tools/Handouts.vue
+++ src/components/Tools/Handouts.vue
@@ -3,17 +3,348 @@
 </script>
 <template>
   <div class="tool">
-    <div class="flex-column">
-      
-      <div class="column-item">
-        <h1>Patient Handouts and Community Links</h1>
-      </div>
 
-    </div>
+    <h1>Patient Handouts and Community Links</h1>
 
-  <p>Click this link for Patient Care Handouts from AHS: <a href="https://myhealth.alberta.ca/Health/aftercareinformation/Pages/default.aspx" target="_blank">PATIENT CARE HANDOUTS</a></p>
+    <p class="lm">Looking for After Care Information? You’re in the right place! It’s the same great information but with a new name - Patient Care Handouts. <br>
+      There are over 2600 topics to choose from. Check back often as new topics are being added all the time.<br>
+    
+      <br>Click this link for Patient Care Handouts from AHS: 
 
-  <p><object data="https://chinookprimarycarenetwork.ab.ca/userfile/file/Getting Connected Booklet June 2022.pdf" type="application/pdf" width="100%" height="800px"></object></p>
+      <a href="https://myhealth.alberta.ca/Health/aftercareinformation/Pages/default.aspx" target="_blank">
+        PATIENT CARE HANDOUTS
+      </a>
+
+    </p>
+
+    <hr>
+    
+    <p>Information linked below from <a href="http://www.pinchercreek.ca/">http://www.pinchercreek.ca/</a></p>
+    <h1 class="Western">Community Food Centre </h1>
+    <p>
+    Emergency food hampers are available, please call or text 403 632 6716 or email foodcentrepc@gmail.ca
+    </p>
+    <p>
+    Contact information for the Community Food Centre is:<br>
+    Phone 403-632-6716<br>
+    Physical address is Unit B  1034 Bev McLachlin Drive (beside The Nest) we are not open for public access.<br>
+    <br>
+    Facebook:https://www.facebook.com/foodcentrepc/ <br>
+    Website: www.pinchercreekcommunityfoodcentre.ca 
+  </p>
+  <h1 class="western2" align="center">
+		<strong>Need Food?</strong></h1>
+		<h4 class="western2" align="center"><strong>Call, text or email us –
+		It’s that simple. </strong>
+		</h4>
+		<p align="center"><strong><a href="tel:+14036326716">Call Us</a></strong></p>
+		<p align="center"><strong><a href="tel:+14036276716">Text - (403)
+		632-6716</a></strong></p>
+		<p align="center"><strong>You must be a resident of the Town or MD
+		of Pincher Creek and that’s it. Your text or call will be
+		answered in a timely manner to discuss your needs with you. </strong>
+		</p>
+		<p align="center"><strong>There is a one-time, sign-up process that
+		is done over the phone. We deliver.</strong></p>
+  <br>
+    <hr>
+    <h1 class="western">
+      Family and Community Support Services (FCSS)</h1>
+
+      <p><strong>The Pincher Creek and District Family and Community
+      Support Services</strong>&nbsp;facilitates preventative social
+      services and community initiatives which contribute to quality of
+      life and family wellness in the communities of the Town of Pincher
+      Creek, the Municipal District of Pincher Creek #9 and the Village of
+      Cowley.</p>
+
+      <h3 class="western">What is FCSS?</h3>
+      <p>Family &amp; Community Support Services (FCSS) is a joint
+      municipal/provincial funding program established to develop, support,
+      and fund preventive social services.<br/>
+      <br/>
+      FCSS is an 80/20
+      funding partnership between the Government of Alberta, municipalities
+      and Metis Settlements. Provincially,&nbsp;the FCSS Program receives
+      its mandate from the <em>Family and Community Support Services
+      Act</em>&nbsp;and<em>&nbsp;Regulation.</em></p>
+      <p>Under FCSS, communities design and deliver social programs that
+      are preventive in nature to promote and enhance well-being among
+      individuals, families, and communities. The programs depend on
+      community resources, often involving volunteers in management and
+      delivery.</p>
+      <p>The FCSS philosophy is based on a belief that self-help
+      contributes to a sense of integrity, self-worth and independence. The
+      developed programs are intended to help individuals in their
+      community to adopt healthy lifestyles, thereby improving the quality
+      of life and building the capacity to prevent and/or deal with crisis
+      situations should they arise.</p>
+      <p>One of the key principles of the FCSS Program is
+      local&nbsp;responsibility in priority-setting and resource
+      allocation. FCSS emphasizes local decision-making for programming,
+      based on the belief that communities have the desire, energy and
+      resources to build community capacity. Each municipality or Metis
+      Settlement, therefore,&nbsp;determines how the dollars they receive
+      should be allocated to best meet local&nbsp;needs.</p>
+
+      <h3 class="western">Who benefits from FCSS?</h3>
+      <p>Programs funded by FCSS provide preventive services to all age
+      groups and people from all walks of life, such as family counselling
+      for persons in distress, care for isolated seniors, and support for
+      children and youth.</p>
+
+      <h3 class="western">Why focus on prevention?</h3>
+      <p>Research shows that an investment of $1 in preventive services
+      yields a $7 return in avoided costs such as police, justice,
+      additional treatment and increased productivity in employment and
+      contribution to society.</p>
+
+      <h3 class="western">Who manages FCSS programs?</h3>
+      <p>The Town of Pincher Creek FCSS manages the FCSS funding programs
+      for the Town, the Municipal District of Pincher Creek #9, and the
+      Village of Cowley. The Town also collaborates with agencies and the
+      community to better understand the magnitude of social issues and how
+      best to respond to them.</p>
+      <p>The FCSS Board of Directors, with representation from the Town of
+      Pincher Creek, Municipal District of Pincher Creek, Cowley, and four
+      members at large from the community, reviews funding requests from
+      social agencies and selects projects for funding.</p>
+
+      <h3 class="western">What types of programs and services will FCSS
+      fund?</h3>
+      <p>In addition to being consistent with the FCSS Act, programs and
+      services must:</p>
+      <ul>
+        <li><p style="margin-bottom: 0in">Be consistent with the funding
+        priorities of the Town and Municipal District of Pincher Creek and
+        the Village of Cowley 
+        </p></li>
+        <li><p style="margin-bottom: 0in">Be collaborative and not duplicate
+        existing programs 
+        </p></li>
+        <li><p style="margin-bottom: 0in">Involve volunteers 
+        </p></li>
+        <li><p style="margin-bottom: 0in">Leverage additional funds to
+        assist with program costs 
+        </p></li>
+        <li><p style="margin-bottom: 0in">State objectives in measurable
+        terms 
+        </p></li>
+        <li><p style="margin-bottom: 0in">Demonstrate the capacity to
+        achieve stated outcomes 
+        </p></li>
+        <li><p>Demonstrate good governance and administration 
+        </p></li>
+      </ul>
+
+      <h3 class="western"><span style="background: #ffff00">2023 Funding
+      Application</span></h3>
+      <ul>
+        <li><p style="margin-bottom: 0in"><a href="http://www.pinchercreek.ca/docs/files/2023%20FCSS%20Application.docx">Application&nbsp;</a>
+          </p></li>
+        <li><p><a href="http://www.pinchercreek.ca/docs/files/2023%20FCSS%20Budget%20template.xlsx">Budget
+        template</a> 
+        </p></li>
+      </ul>
+      <p style="margin-bottom: 0in"><br/>
+      Deadling is September 14 at 4
+      p.m.&nbsp;<br/>
+      <br/>
+      (Clicking on these links will download the file
+      to your computer)</p>
+
+      <h3 class="western">Additional Information</h3>
+      <p><strong>Needs Assessments</strong> 
+      </p>
+      <ul>
+        <li><p style="margin-bottom: 0in"><a href="http://www.pinchercreek.ca/docs/files/Pincher%20Creek%20Day%20Care-%20Community%20FAQ%5C%27s.pdf" onclick="window.open(this.href, '', 'resizable=no,status=no,location=no,toolbar=no,menubar=no,fullscreen=no,scrollbars=no,dependent=no'); return false;">Child
+        Care Needs Assesment&nbsp;</a> 
+        </p></li>
+        <li><p style="margin-bottom: 0in"><a href="http://www.pinchercreek.ca/docs/files/Pincher%20Creek%20Proposal%20September%202017%20-%20Final%20Draft.pdf">Child
+        Care Proposal September 2017</a> 
+        </p></li>
+        <li><p style="margin-bottom: 0in"><a href="http://www.pinchercreek.ca/docs/files/Final%20Report%20Pincher%20Creek%20FCSS%20-%20Social%20Needs%20Assessment.pdf">Social
+        Needs Assesment March 2020</a> 
+        </p></li>
+        <li><p><a href="http://www.pinchercreek.ca/docs/files/December%2021%20final%20version%20Pincher%20Creek%20CHNA%20Gabrielle%20Kirk.pdf">Community
+        Health Needs Assessment</a> 
+        </p></li>
+      </ul>
+      <p><br/>
+      <strong>Provincial Websites</strong> 
+      </p>
+      <ul>
+        <li><p style="margin-bottom: 0in"><a href="http://www.child.alberta.ca/home/1022.cfm">Alberta
+        Human Services</a> 
+        </p></li>
+        <li><p><a href="http://www.fcssaa.ab.ca/">Family and Community
+        Support Services Association of Alberta</a> 
+        </p></li>
+      </ul>
+
+      <h3 class="western">Contact Information</h3>
+      <p>The Pincher Creek and District FCSS office is located at the&nbsp;Town
+      of Pincher Creek Administrative Office (962 St. John Avenue)</p>
+      <p>FCSS Coordinator:&nbsp;<a href="mailto:fcss@pinchercreek.ca?subject=Website%20Enquiry">A</a><a href="mailto:fcss@pinchercreek.ca?subject=Message%20from%20the%20Town%20website%20">ndrea
+      Hlady&nbsp;</a></p>
+      <table cellpadding="0" cellspacing="0">
+        <tr>
+          <td style="border: none; padding: 0in"><p>Mailing Address:</p>
+          </td>
+          <td style="border: none; padding: 0in"><p>&nbsp;</p>
+          </td>
+          <td style="border: none; padding: 0in"><p>Box 2841<br/>
+      Pincher
+            Creek, Alberta&nbsp;<br/>
+      T0K 1W0</p>
+          </td>
+        </tr>
+        <tr>
+          <td style="border: none; padding: 0in"><p>Phone:</p>
+          </td>
+          <td style="border: none; padding: 0in"><p>&nbsp;</p>
+          </td>
+          <td style="border: none; padding: 0in"><p>(403) 627-3156</p>
+          </td>
+        </tr>
+        <tr>
+          <td style="border: none; padding: 0in"><p>Email:</p>
+          </td>
+          <td style="border: none; padding: 0in"><p>&nbsp;</p>
+          </td>
+          <td style="border: none; padding: 0in"><p><a href="mailto:fcss@pinchercreek.ca">fcss@pinchercreek.ca</a></p>
+          </td>
+        </tr>
+      </table>
+      <p style="line-height: 100%; margin-bottom: 0in"><br/>
+
+      </p>
+
+      <hr/>
+      <h1 class="western">
+      Community Links</h1>
+    <p>
+    <a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1116334#contentStart">Home
+    Care Services</a> 
+    </p>
+    <p>1222 Bev McLachlin Drive, Pincher Creek 
+    </p>
+    <p>&nbsp;<strong>Phone:&nbsp;</strong><a href="http://tel:4036271230/">403-627-1230</a>&nbsp;
+    &nbsp;<strong>Tollfree:&nbsp;</strong><a href="http://tel:8663886380/">1-866-388-6380</a>&nbsp;
+    </p>
+    <p>&nbsp;<strong>Distance&nbsp;</strong>0.96 km&nbsp; <a href="http://maps.google.com/maps/?daddr=1222%20Bev%20McLachlin%20%20Drive%20,%20Pincher%20Creek">Get
+    Directions</a> 
+    </p>
+    <hr/>
+
+    <p><a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1076072#contentStart">Public
+    Health Nursing</a> 
+    </p>
+    <p>1222 Bev McLachlin Drive, Pincher Creek 
+    </p>
+    <p>&nbsp;<strong>Phone:&nbsp;</strong><a href="http://tel:4036271230/">403-627-1230</a>&nbsp;
+    </p>
+    <p>&nbsp;<strong>Distance&nbsp;</strong>0.96 km&nbsp; <a href="http://maps.google.com/maps/?daddr=1222%20Bev%20McLachlin%20%20Drive%20,%20Pincher%20Creek">Get
+    Directions</a> 
+    </p>
+    <hr/>
+
+    <p><a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1125355#contentStart">Labour
+    Delivery and Maternal Child Services</a> 
+    </p>
+    <p>1222 Bev McLachlin Drive, Pincher Creek 
+    </p>
+    <p>&nbsp;<strong>Phone:&nbsp;</strong><a href="http://tel:4036271234/">403-627-1234
+    (Switchboard)</a>&nbsp; 
+    </p>
+    <p>&nbsp;<strong>Distance&nbsp;</strong>0.96 km&nbsp; <a href="http://maps.google.com/maps/?daddr=1222%20Bev%20McLachlin%20%20Drive%20,%20Pincher%20Creek">Get
+    Directions</a> 
+    </p>
+    <hr/>
+
+    <p><a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1116332#contentStart">Home
+    Care Services</a> 
+    </p>
+    <p>12501 20 Avenue, Blairmore 
+    </p>
+    <p>&nbsp;<strong>Phone:&nbsp;</strong><a href="http://tel:4035625030/">403-562-5030</a>&nbsp;
+    &nbsp;<strong>Tollfree:&nbsp;</strong><a href="http://tel:8663886380/">1-866-388-6380</a>&nbsp;
+    </p>
+    <p>&nbsp;<strong>Distance&nbsp;</strong>38.6 km&nbsp; <a href="http://maps.google.com/maps/?daddr=12501%2020%20Avenue%20,%20Blairmore">Get
+    Directions</a> 
+    </p>
+    <hr/>
+
+    <p><a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1104101#contentStart">Public
+    Health Nursing</a> 
+    </p>
+    <p>12501 20 Avenue, Blairmore 
+    </p>
+    <p>&nbsp;<strong>Phone:&nbsp;</strong><a href="http://tel:4035625030/">403-562-5030</a>&nbsp;
+    </p>
+    <p>&nbsp;<strong>Distance&nbsp;</strong>38.6 km&nbsp; <a href="http://maps.google.com/maps/?daddr=12501%2020%20Avenue%20,%20Blairmore">Get
+    Directions</a> 
+    </p>
+    <hr/>
+
+    <p><a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1125354#contentStart">Labour
+    Delivery and Maternal Child Services</a> 
+    </p>
+    <p>2001 107 Street, Blairmore 
+    </p>
+    <p>&nbsp;<strong>Phone:&nbsp;</strong><a href="http://tel:4035625011/">403-562-5011
+    (Switchboard)</a>&nbsp; 
+    </p>
+    <p>&nbsp;<strong>Distance&nbsp;</strong>40.3 km&nbsp; <a href="http://maps.google.com/maps/?daddr=2001%20107%20Street%20,%20Blairmore">Get
+    Directions</a> 
+    </p>
+    <hr/>
+
+    <p><a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1116333#contentStart">Home
+    Care Services</a> 
+    </p>
+    <p>744 26 Street S, Fort Macleod 
+    </p>
+    <p>&nbsp;<strong>Phone:&nbsp;</strong><a href="http://tel:4035535300/">403-553-5300</a>&nbsp;
+    &nbsp;<strong>Tollfree:&nbsp;</strong><a href="http://tel:8663886380/">1-866-388-6380</a>&nbsp;
+    </p>
+    <p>&nbsp;<strong>Distance&nbsp;</strong>47.9 km&nbsp; <a href="http://maps.google.com/maps/?daddr=744%2026%20Street%20S%20S,%20Fort%20Macleod">Get
+    Directions</a> 
+    </p>
+    <hr/>
+
+    <p><a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1117891#contentStart">Public
+    Health Nursing</a> 
+    </p>
+    <p>744 26 Street S, Fort Macleod 
+    </p>
+    <p>&nbsp;<strong>Phone:&nbsp;</strong><a href="http://tel:4035535351/">403-553-5351</a>&nbsp;
+    </p>
+    <p>&nbsp;<strong>Distance&nbsp;</strong>47.9 km&nbsp; <a href="http://maps.google.com/maps/?daddr=744%2026%20Street%20S%20S,%20Fort%20Macleod">Get
+    Directions</a> 
+    </p>
+    <hr/>
+
+    <p><a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1092941#contentStart">Alberta
+    Aids to Daily Living - Respiratory Benefits Program</a> 
+    </p>
+    <p>Telephone/Web Access Only 
+    </p>
+    <p>&nbsp;<strong>Tollfree:&nbsp;</strong><a href="http://tel:8888088678/">1-888-808-8678</a>&nbsp;
+    </p>
+    <hr/>
+
+    <p><a href="https://social.albertahealthservices.ca/findhealth/Service.aspx?serviceAtFacilityId=1060157#contentStart">Alberta
+    Health Services (AHS) Privacy Office</a> 
+    </p>
+    <p>Telephone/Web Access Only 
+    </p>
+    <p>&nbsp;<strong>Phone:&nbsp;</strong><a href="http://tel:8774769874/">1-877-476-9874
+    </a>&nbsp; 
+    </p>
+    <p style="line-height: 100%; margin-bottom: 0in"><br/>
+
+    </p>
   
   </div>
   
@@ -41,7 +372,27 @@ h1 {
 h2 {
   padding-left: 30px;
 }
-p {
+p.lm {
   padding: 30px;
+  font-size: larger;
 }
+hr {
+  margin-top: 5px;
+}
+p {
+  padding-left: 30px;
+  font-size: larger;
+}
+h1.western { font-family: "Liberation Serif", serif; font-size: 24pt; font-weight: bold }
+		h1.cjk { font-family: "NSimSun"; font-size: 24pt; font-weight: bold }
+		h1.ctl { font-family: "Lucida Sans"; font-size: 24pt; font-weight: bold }
+		h3 { margin-top: 0.1in; margin-bottom: 0.08in; background: transparent; page-break-after: avoid }
+		h3.western { font-family: "Liberation Serif", serif; font-size: 14pt; font-weight: bold }
+    h4.western2 { font-family: "Liberation Serif", serif; font-size: 12pt; font-weight: bold }
+		h3.cjk { font-family: "NSimSun"; font-size: 14pt; font-weight: bold }
+		h3.ctl { font-family: "Lucida Sans"; font-size: 14pt; font-weight: bold }
+		td p { orphans: 0; widows: 0; background: transparent }
+		strong { font-weight: bold }
+		em { font-style: italic }
+		a:link { color: #000080; so-language: zxx; text-decoration: underline }
 </style>
